018cfa57657b1450f87761da54d38558086c0c90
undefinedDen <tunevdenis123@gmail.com>
2025-01-11 12:47:46 +0200

    Create new person and change his/her profession

--- src/app/Person.java
+++ src/app/Person.java
@@ -1,27 +1,30 @@
 package app;
 
 public class Person {
-   private String name;
-   private int age;
-   private String profession;
+    private String name;
+    private int age;
+    private String profession;
 
-   public void setName(String name){
-       this.name = name;
-   }
-   public void setAge(int age){
-       if(age <= 0 || age > 150){
-           System.out.println("Age can not be less or equals 0. " +
-                   "And age can not be bigger than 150 years old");
-       }else {
-           this.age = age;
-       }
-   }
-   public void setProfession(String profession){
-       this.profession = profession;
-   }
-   public String getName(){
-       return name;
-   }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0 || age > 150) {
+            System.out.println("Age can not be less or equals 0. " +
+                    "And age can not be bigger than 150 years old");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void setProfession(String profession) {
+        this.profession = profession;
+    }
+
+    public String getName() {
+        return name;
+    }
 
     public int getAge() {
         return age;
@@ -30,17 +33,25 @@ public class Person {
     public String getProfession() {
         return profession;
     }
-    void displayOutput(String name, int age, String profession){
+
+    void displayOutput(String name, int age, String profession) {
         System.out.println("Name: " + name + ", age: " + age + ", Profession: " + profession);
     }
+
     //Enter value for Scanner
-    void enterName(){
+    void enterName() {
         System.out.print("Enter name: ");
     }
-    void enterAge(){
+
+    void enterAge() {
         System.out.print("Enter age: ");
     }
-    void enterProfession(){
+
+    void enterProfession() {
         System.out.print("Enter profession: ");
+    }
+
+    void changeProfession(String newProfession) {
+        profession = newProfession;
     }
 }
