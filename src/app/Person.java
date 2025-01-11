129f4c86d9891731869d9c335c72dfbc7f972301
undefinedDen <tunevdenis123@gmail.com>
2025-01-11 10:52:03 +0200

    into class Person added setters and getters

--- src/app/Person.java
+++ src/app/Person.java
@@ -1,4 +1,33 @@
 package app;
 
 public class Person {
+   private String name;
+   private int age;
+   private String profession;
+
+   public void setName(String name){
+       this.name = name;
+   }
+   public void setAge(int age){
+       if(age <= 0 || age > 150){
+           System.out.println("Age can not be less or equals 0. " +
+                   "And age can not be bigger than 150 years old");
+       }else {
+           this.age = age;
+       }
+   }
+   public void setProfession(String profession){
+       this.profession = profession;
+   }
+   public String getName(){
+       return name;
+   }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getProfession() {
+        return profession;
+    }
 }
